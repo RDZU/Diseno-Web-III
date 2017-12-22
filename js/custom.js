243295a733f867d26a26daf0c95116ff5a66fbb4
Randolph David <davidzamora@gmail.com>
2017-12-22 11:41:02 -0400

    section team

--- js/custom.js
+++ js/custom.js
@@ -13,3 +13,18 @@ $(document).ready(function(){
     });
 
 });
+                        
+/*===============================================
+                        Team
+================================================*/
+$(document).ready(function(){
+
+    $("#team-members").owlCarousel({
+        items: 3,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true
+    });
+
+});
