1043e1e22779f93c2de01e8e76c6556a34244fa3
Randolph David <davidzamora@gmail.com>
2017-12-23 10:47:20 -0400

    finalmaquetado

--- js/custom.js
+++ js/custom.js
@@ -44,4 +44,81 @@ $(document).ready(function(){
                 
             });
                             
-                          
+                          /*===============================================
+                            Stats
+    ================================================*/
+    $(document).ready(function(){
+
+        $(".counter").counterUp({
+            delay: 10,
+            time: 3000
+        });
+
+    });
+                        
+                    /*===============================================
+                            Stats
+    ================================================*/
+    $(document).ready(function(){
+
+        $(".counter").counterUp({
+            delay: 10,
+            time: 3000
+        });
+
+    });
+                        
+                  /*===============================================
+                                Clients
+        ================================================*/
+        $(document).ready(function(){
+
+            $("#clients-list").owlCarousel({
+                items: 6,
+                autoplay: true,
+                smartSpeed: 700,
+                loop: true,
+                autoplayHoverPause: true
+            });
+
+        });
+                        
+
+
+
+/*===============================================
+                        Smooth-Scroll
+================================================*/
+
+$("a.smooth-scroll").click( function(evento){
+
+    evento.preventDefault();
+    var section = $(this).attr("href");
+
+    $("html, body").animate({
+        scrollTop: $(section).offset().top
+    });
+
+},);
+           
+
+
+/*===============================================
+                        Button Top
+================================================*/
+
+$(function(){
+
+    $(window).scroll(function() {
+
+        if($(this).scrollTop() < 50) {
+            $("#back-to-top").fadeOut();
+        }else{
+            $("#back-to-top").fadeIn();
+        }
+
+    });
+
+});
+                        
+                    
